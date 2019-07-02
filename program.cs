4670ed2f7226dd0605664341a775a211dba038ce
Olivier <30650508+OSCHOCKA@users.noreply.github.com>
2019-07-02 23:52:33 +0200

    Correction de la méthode WriteLine erreur de frappe

--- program.cs
+++ program.cs
@@ -14,13 +14,9 @@ namespace KM_Service_MergeDocs
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new Service1()
-            };
-            ServiceBase.Run(ServicesToRun);
-            Console.Writeline("hello world");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
