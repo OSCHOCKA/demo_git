8d076aa4c1042e7522ebf6b0064b6391bf11b417
Olivier <30650508+OSCHOCKA@users.noreply.github.com>
2019-07-02 23:53:56 +0200

    suppression des using inutiles

--- program.cs
+++ program.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KM_Service_MergeDocs
 {
